02dcd29605110081fa1bc63f65d57aeec245e647
Elton Santos <elton.santos07@outlook.com>
2021-05-27 11:59:38 -0300

    Criação do método DELETE

--- blogPessoal/src/main/java/br/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/br/org/generation/blogPessoal/controller/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,10 @@ public class PostagemController {
 	@PutMapping
 	public ResponseEntity<Postagem> putPostagem (@RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem (@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
